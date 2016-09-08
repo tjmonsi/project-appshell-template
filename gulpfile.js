0059c3bb14ebee5855ca48b58c90f3d2116ba085
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2016-09-08 13:55:31 +0000

    added skeleton for css-compile

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,12 @@ var gulp = require('gulp');
 gulp.task('default', function() {
   // place code for your default task here
 });
+
+gulp.task('css-compile', function() {
+    
+});
+
+var watcher = gulp.watch('src/css/*.sass', ['css-compile']);
+watcher.on('change', function(event) {
+  console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+});
